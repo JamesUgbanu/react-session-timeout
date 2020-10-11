b139a480fc27e431a95cb26dda5a9723f85314f4
James Ugbanu <jamesugbanu@gmail.com>
2020-10-11 11:08:41 +0100

    react-session-timeout tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  Container,
+  Box,
+  Button,
+  makeStyles
+} from '@material-ui/core';
+import logo from './logo.svg'
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  cardContainer: {
+    paddingBottom: 80,
+    paddingTop: 80,
+  },
+}));
+
+const LoginButton = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+        color="secondary"
+        data-testid="submit"
+        fullWidth
+        size="large"
+        onClick={props.onClick}
+        variant="contained"
+      >
+        Log In
+      </Button>
+  );
+}
+
+const LogoutButton = (props) => {
+
+  return (
+        <Button
+        color="secondary"
+        data-testid="submit"
+        fullWidth
+        size="large"
+        onClick={props.onClick}
+        variant="contained"
+      >
+        Logout
+      </Button>
+  );
+}
+
+
+const App = () => {
+  const classes = useStyles();
+  const [isAuthenticated, setAuth] = useState(false);
+  let button;
+
+    const handleClick = () => {
+      setAuth(!isAuthenticated);
+      console.log('hi')
+    }
+  
+    if (isAuthenticated) {
+      button = <LogoutButton onClick={handleClick}/>;
+    } else {
+      button = <LoginButton onClick={handleClick} />;
+    }
+
+  return (
+    <div className={classes.root}>
+    <Container
+        className={classes.cardContainer}
+        maxWidth="sm"
+      >
+        <Box
+          mb={4}
+          display="flex"
+          justifyContent="center"
         >
-          Learn React
-        </a>
-      </header>
+           <img
+          alt="Logo"
+          src={logo}
+          />
+        </Box>
+        <Box
+          mb={8}
+          display="flex"
+          justifyContent="center"
+        >
+          <Box mt={2}>
+              {button}
+            </Box>
+     </Box>
+    </Container>       
     </div>
   );
 }
